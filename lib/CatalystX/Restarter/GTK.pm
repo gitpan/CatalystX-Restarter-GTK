01c01f69d4e05ff68086e92402f2907d6be4408a
Dhaval Dhanani <dhaval@cpan.org>
2012-04-27 09:06:12 +0000

    Import of DHAVAL/CatalystX-Restarter-GTK-0.07 from CPAN.
    gitpan-cpan-distribution: CatalystX-Restarter-GTK
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         DHAVAL/CatalystX-Restarter-GTK-0.07.tar.gz
    gitpan-cpan-author:       DHAVAL
    gitpan-cpan-maturity:     released

--- lib/CatalystX/Restarter/GTK.pm
+++ lib/CatalystX/Restarter/GTK.pm
@@ -12,7 +12,7 @@ use Socket               qw(AF_UNIX SOCK_STREAM);
 use IO::Handle           qw();
 use namespace::autoclean;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 extends 'Catalyst::Restarter';
 
@@ -503,7 +503,7 @@ You can also create a shell script and add a shortcut to panel. This avoids need
     export CATALYST_RESTARTER=CatalystX::Restarter::GTK
     perl script/myapp_server.pl -r 
 
-To use this restarter for specific application only, set appropirate envioronment variable. 
+To use this restarter for specific application only, set appropriate envioronment variable. 
 
     MYAPP_RESTARTER=CatalystX::Restarter::GTK
  
